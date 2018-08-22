d7536e992898bf15ba68938fd9b3d0dd483024af
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2018-08-22 08:26:56 +0200

    Java 11.

--- saga-samples/polyglot-persistence/src/main/java/module-info.java
+++ saga-samples/polyglot-persistence/src/main/java/module-info.java
@@ -4,7 +4,6 @@ module no.ssb.saga.samples.polyglot {
     requires no.ssb.saga.serialization;
     requires no.ssb.concurrent.futureselector;
     requires java.base;
-    requires jdk.incubator.httpclient;
     requires undertow.core;
     requires json;
 }
--- saga-samples/polyglot-persistence/src/test/java/no/ssb/saga/samples/polyglot/PolyglotMainTest.java
+++ saga-samples/polyglot-persistence/src/test/java/no/ssb/saga/samples/polyglot/PolyglotMainTest.java
@@ -1,8 +1,5 @@
 package no.ssb.saga.samples.polyglot;
 
-import jdk.incubator.http.HttpClient;
-import jdk.incubator.http.HttpRequest;
-import jdk.incubator.http.HttpResponse;
 import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -10,6 +7,9 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 
 public class PolyglotMainTest {
@@ -22,10 +22,10 @@ public class PolyglotMainTest {
         HttpRequest httpRequest = HttpRequest
                 .newBuilder(new URI("http://127.0.0.1:8342/test1"))
                 .header("Content-Type", "application/octet-stream")
-                .PUT(HttpRequest.BodyPublisher.fromString("Hello Polyglot", StandardCharsets.UTF_8))
+                .PUT(HttpRequest.BodyPublishers.ofString("Hello Polyglot", StandardCharsets.UTF_8))
                 .build();
 
-        HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandler.asString(StandardCharsets.UTF_8));
+        HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
         Assert.assertEquals(response.statusCode(), 201);
         String json = response.body();
         System.out.println(new JSONObject(json).toString(2));
